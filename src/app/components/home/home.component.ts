5c3e2299688429a75e792c031878519794088c50
swnai <swnai@eg.dk>
2024-05-30 19:19:30 +0530

    Third Commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { LComponentComponent } from '../content/l-component/l-component.component';
-import { RComponentComponent } from '../content/r-component/r-component.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { HeaderComponent } from '../header/header.component';
-{HeaderComponent}
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LComponentComponent, RComponentComponent, SidebarComponent, HeaderComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
