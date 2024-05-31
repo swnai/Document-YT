8bca698176e7004792fe6a298801e82aaca34fa2
swnai <swnai@eg.dk>
2024-05-31 07:56:00 +0530

    Fourth Commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { toArray } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export  class HomeComponent {
+  card1 = async (idd: string) => {
+    console.log(idd);
+    const data = await fetch(`http://localhost:3000/books/${idd}`).then((res) =>
+      res.json()
+    );
+    const { id, title,descr,pub_date, author, img, pdf } = data;
+
+    const authors = document.getElementById('author');
+    const descrp = document.getElementById('descr');
+    const pd = document.getElementById('pd');
+    const pdfs = document.getElementById('pdf');
+    const d_header=document.getElementById('document_header')
+ 
+    if (authors) {
+      authors.innerText = 'Author: ' + author;
+    }
+    if (descrp) {
+      descrp.innerText = 'Description: ' + descr;
+    }
+    if (pd) {
+      pd.innerText = 'Publish Date: ' + pub_date;
+    }
+    if (pdfs) {
+      pdfs.setAttribute('src', pdf);
+    }
+    if(d_header){
+      d_header.innerText = title
+    }
+ 
+    console.log(data);
+  };
 }
